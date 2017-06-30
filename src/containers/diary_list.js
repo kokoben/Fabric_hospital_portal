3405874841607813f7d9cf23416266a39731d721
Ben <benzuvsteel@gmail.com>
2017-06-30 06:04:58 -0700

    implemented first pass of grid view. need to format image thumbnails.

--- src/containers/diary_list.js
+++ src/containers/diary_list.js
@@ -2,15 +2,22 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class DiaryList extends Component {
-	renderDiary(diaryData) {
+	renderDiary(diaryData, key=diaryData.dishId /*not sure if which specific field used as key matters..*/) {
 		return (
-			<div>
-				<ul>
-					<li>Dish Name: {diaryData.dishName}</li>
-					<li>Price :{diaryData.price}</li>
-					<li>Calories: {diaryData.calories}</li>
-					<li>Health Score: {diaryData.healthScoreM}</li>
-				</ul>
+			<div key={diaryData.dishId}>
+				<div className="col-sm-4" >
+					<ul>
+						<li><img src={diaryData.imageLink} /></li>
+						<li>Dish Name: {diaryData.dishName}</li>
+						<li>Price :{diaryData.price}</li>
+						<li>Calories: {diaryData.calories}</li>
+						<li>Health Score: {diaryData.healthScoreM}</li>
+						<li>{key}</li>
+					</ul>
+				</div>
+				{key % 3 == 2 &&
+					<div className="clearfix"></div> 
+				}
 			</div>
 		)
 	}
