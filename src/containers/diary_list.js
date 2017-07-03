f898728c1d91cd775026c29ba3f8a9fa4598998f
Ben <benzuvsteel@gmail.com>
2017-07-02 22:39:54 -0700

    implemented dynamic clearfixing of dish grid.

--- src/containers/diary_list.js
+++ src/containers/diary_list.js
@@ -2,6 +2,28 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 class DiaryList extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {width: $(window).width(), 
+						height: $(window).height()
+		}
+
+		this.updateDimensions = this.updateDimensions.bind(this);
+	}
+
+	componentDidMount() {
+		window.addEventListener("resize",  this.updateDimensions);
+	}
+	
+	componenetWillUnmount() {
+		window.removeEventListener("resize", this.updateDimensions);
+	}
+	
+	updateDimensions() {
+		this.setState({width: $(window).width(), height: $(window).height()});
+	}
+
 	renderDiary(diaryData, key=diaryData.dishId /*not sure if which specific field used as key matters..*/) {
 		return (
 			<div key={diaryData.dishId}>
@@ -14,6 +36,18 @@ class DiaryList extends Component {
 						<li className="list-group-item">Health Score: {diaryData.healthScoreM}</li>
 					</ul>
 				</div>
+				{($(window).width() >= 576 && $(window).width() < 768) &&
+					key % 2 == 1 && 
+						<div className="clearfix visible-sm-block"></div>
+				}
+				{($(window).width() >= 768 && $(window).width() < 992) &&
+					key % 3 == 2 &&
+						<div className="clearfix visible-md-block"></div>
+				}
+				{$(window).width() >= 992 &&
+					key % 4 == 3 &&
+						<div className="clearfix visible-lg-block"></div>
+				}
 			</div>
 		)
 	}
